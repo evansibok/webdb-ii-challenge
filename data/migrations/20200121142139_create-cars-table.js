a7020fa92871a1b3f7fda19a011006d1ee54171c
Evans Ibok <evansibok@gmail.com>
2020-01-21 14:36:55 +0100

    add configuration to make file

--- data/migrations/20200121142139_create-cars-table.js
+++ data/migrations/20200121142139_create-cars-table.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
-  
+  // Create Cars table
+  return knex.schema.createTable('car', table => {
+    table.increments(); // For id/primary key column, auto increments
+    table.string('make').notNUllable();
+    table.string('model').notNUllable();
+    table.integer('VIN').notNUllable();
+    table.integer('mileage').notNUllable();
+    table.string('transmissionType');
+    table.string('status');
+  })
 };
 
 exports.down = function(knex) {
-  
+  // Destroy Cars table
+  return knex.schema.dropTableIfExists('car');
 };
